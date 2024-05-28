21605d2fd3f529f625c4fd8eb11f763ed45b7369
xsalles <xs.salles@gmail.com>
2024-05-28 13:02:20 -0300

    feat: styles of input and change the background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import './App.css'
+import Fundo from './assets/fundo.jpg'
 
 export default function App() {
   return (
-    <div className="title">
-      <h1>Buscador de CEP</h1>
-      <div className="formulario">
-        <form>
-          <input placeholder='Seu CEP' type="text" />
-          <input placeholder='Estado' type="text" />
-          <input placeholder='Cidade' type="text" />
-          <input placeholder='Bairro' type="text" />
-          <input placeholder='Rua' type="text" />
-          <input placeholder='Número' type="text" />
-        </form>
+    
+    <div className="CEP">
+      
+      <div className="title">
+        <h1>Buscador de CEP</h1>
+      </div>
+      <div className="form">
+        <div className="itens-form">
+          <form>
+            <input placeholder='Seu CEP' type="text" />
+          </form>
+          <form>
+            <input placeholder='Estado' type="text" />
+          </form>
+          <form>
+            <input placeholder='Cidade' type="text" />
+          </form>
+          <form>
+            <input placeholder='Bairro' type="text" />
+          </form>
+          <form>
+            <input placeholder='Rua' type="text" />
+          </form>
+          <form>
+            <input placeholder='Número' type="text" />
+          </form>
+        </div>
       </div>
     </div>
 
