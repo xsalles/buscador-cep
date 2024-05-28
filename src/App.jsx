1314abb2e606415a6451bfeb960b6e9db6dc51e0
xsalles <xs.salles@gmail.com>
2024-05-28 05:46:40 -0300

    feat: title and form

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="title">
+      <h1>Buscador de CEP</h1>
+      <div className="formulario">
+        <form>
+          <input placeholder='Seu CEP' type="text" />
+          <input placeholder='Estado' type="text" />
+          <input placeholder='Cidade' type="text" />
+          <input placeholder='Bairro' type="text" />
+          <input placeholder='Rua' type="text" />
+          <input placeholder='Número' type="text" />
+        </form>
+      </div>
+    </div>
+
+
+
+
+  )
+
+}
+
+
