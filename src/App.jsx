3109e00bd74317e4df62b841ffcf4e68f045aa8e
xsalles <xs.salles@gmail.com>
2024-06-17 12:47:21 -0300

    reactored: background's size and added folder components and started customizing the site´s responsives

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,12 @@
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cep from './components'
+import "../src/components/styles.css"
 
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <Cep title="FORMULÁRIO DE CEP" nav="Buscador de CEP" git="Git Hub" insta="Instagram do Dev" linkedin="LinkedIn" 
+    api="Site contribuinte" email="xs.salles@gmail.com" endName="Pedro Henrique Moreira Sales"/>
 
-
-export default function Cep(props) {
-    return (
-
-        <div className="cep">
-            <nav className="nav">
-                <h1>{props.nav}</h1>
-                <a href="https://viacep.com.br/">{props.api}</a>
-                <a href="https://www.instagram.com/xs.salles/?next=%2F">{props.insta}</a>
-                <a href="https://www.linkedin.com/in/pedro-sales-00090a274/">{props.linkedin}</a>
-                <a href="https://github.com/xsalles">{props.git}</a>
-            </nav>
-            <div className="title">
-                <h1>{props.title}</h1>
-            </div>
-            <div className="form">
-                <div className="itens-form">
-                    <form>
-                        <input placeholder='Seu CEP' type="text" />
-                    </form>
-                    <form>
-                        <input placeholder='Estado' type="text" />
-                    </form>
-                    <form>
-                        <input placeholder='Cidade' type="text" />
-                    </form>
-                    <form>
-                        <input placeholder='Bairro' type="text" />
-                    </form>
-                    <form>
-                        <input placeholder='Rua' type="text" />
-                    </form>
-                    <form>
-                        <input placeholder='Número' type="text" />
-                    </form>
-                    <form>
-                        <input type="submit"  />
-                    </form>
-
-                </div>
-            </div>
-            <footer className="end">
-                <h2>{props.endName}</h2>
-                <div className="line"></div>
-                <p>Contato: <a href="mailto:xs.salles@gmail.com">{props.email}</a></p>
-            </footer>
-        </div>
-    )
-
-}
-
-
+)
---
+++ src/components/index.jsx
@@ -0,0 +1,52 @@
+import './styles.css'
+
+export default function Cep(props) {
+    return (
+
+        <div className="cep">
+            <nav className="nav">
+                <h1>{props.nav}</h1>
+                <a href="https://viacep.com.br/">{props.api}</a>
+                <a href="https://www.instagram.com/xs.salles/?next=%2F">{props.insta}</a>
+                <a href="https://www.linkedin.com/in/pedro-sales-00090a274/">{props.linkedin}</a>
+                <a href="https://github.com/xsalles">{props.git}</a>
+            </nav>
+            <div className="form">
+                <div className="itens-form">
+                <div className="title">
+                <h1>{props.title}</h1>
+            </div>
+                    <form>
+                        <input placeholder='Seu CEP' type="text" />
+                    </form>
+                    <form>
+                        <input placeholder='Estado' type="text" />
+                    </form>
+                    <form>
+                        <input placeholder='Cidade' type="text" />
+                    </form>
+                    <form>
+                        <input placeholder='Bairro' type="text" />
+                    </form>
+                    <form>
+                        <input placeholder='Rua' type="text" />
+                    </form>
+                    <form>
+                        <input placeholder='Número' type="text" />
+                    </form>
+                    <form>
+                        <input type="submit"  />
+                    </form>
+                </div>
+            </div>
+            <footer className="end">
+                <h2>{props.endName}</h2>
+                <div className="line"></div>
+                <p>Contato: <a href="mailto:xs.salles@gmail.com">{props.email}</a></p>
+            </footer>
+        </div>
+    )
+
+}
+
+
