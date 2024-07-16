4617bde5fb88740ca86cc80fe7fca25d8362a2a2
xsalles <xs.salles@gmail.com>
2024-07-16 11:48:13 -0300

    feat: used auto-complete when somepeople types her/him cepto complete other inputs, like: 'estado', 'rua'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import "../src/components/styles.css";
 import Cep from './components';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Cep  title="FORMULÁRIO DE CEP" nav="Buscador de CEP" git="Git Hub" insta="Instagram do Dev" linkedin="LinkedIn" 
-    api="Site contribuinte" email="xs.salles@gmail.com" endName="Pedro Henrique Moreira Sales"/>
+    <Cep />
 );
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,46 +1,73 @@
+import { useState } from 'react'
 import './styles.css'
-import { useForm } from 'react-hook-form'
+
     
 
-export default function Cep(props) {
-    const checkCep = (e) => {
-        const cep = e.target.value.replace(/\D/g, '');
-        console.log(cep);
-        fetch(`viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
-            console.log(data);
-        })
+export default function Cep() {
+
+    const [cep, setCep] = useState('');
+    const [estado, setEstado] = useState('');
+    const [rua, setRua] = useState('');
+    const [bairro, setBairro] = useState('');
+    const [cidade, setCidade] = useState('');
+
+    const handleCepChange = async (e) => {
+        const cepValue = e.target.value;
+        setCep(cepValue);
+
+        if(cepValue.length === 8) {
+            try {
+                const response = await fetch(`https:/viacep.com.br/ws/${cepValue}/json/`);
+                const data = await response.json();
+
+                if(!data.erro) {
+                    setEstado(data.uf);
+                    setBairro(data.bairro);
+                    setCidade(data.localidade);
+                    setRua(data.logradouro);
+                } else {
+                    alert('Cep não encontrado')
+                }
+            } catch (error) {
+                console.error('O cep não foi encontrado', error);
+                alert('O cep não foi encontrado!');
+            }
+        }
     }
+
+    
+   
     return(
         <div className="cep">
             <nav className="nav">
-                <h1>{props.nav}</h1>
-                <a href="https://viacep.com.br/">{props.api}</a>
-                <a href="https://www.instagram.com/xs.salles/?next=%2F">{props.insta}</a>
-                <a href="https://www.linkedin.com/in/pedro-sales-00090a274/">{props.linkedin}</a>
-                <a  href="https://github.com/xsalles">{props.git}</a>
+                <h1>Buscador de cep</h1>
+                <a href="https://viacep.com.br/">ViaCep</a>
+                <a href="https://www.instagram.com/xs.salles/?next=%2F">Instagram</a>
+                <a href="https://www.linkedin.com/in/pedro-sales-00090a274/">Linkedin</a>
+                <a  href="https://github.com/xsalles">GitHub</a>
             </nav>
             <div className="form">
                 <div className="itens-form">
                 <div className="title">
-                <h1>{props.title}</h1>
+                <h1>FORMULÁRIO DE CEP</h1>
             </div>
                     <form>
-                        <input placeholder='Seu CEP' type="text" {...register("cep")} onblur={checkCep} />
+                        <input placeholder='Seu CEP' type="text" value={cep} onChange={handleCepChange}/>
                     </form>
                     <form>
-                        <input placeholder='Estado' type="text" />
+                        <input placeholder='Estado' type="text" value={estado} readOnly />
                     </form>
                     <form>
-                        <input placeholder='Cidade' type="text" />
+                        <input placeholder='Cidade' type="text" value={cidade} readOnly />
                     </form>
                     <form>
-                        <input placeholder='Bairro' type="text" />
+                        <input placeholder='Bairro' type="text" value={bairro} readOnly />
                     </form>
                     <form>
-                        <input placeholder='Rua' type="text" />
+                        <input placeholder='Rua' type="text" value={rua} readOnly/>
                     </form>
                     <form>
-                        <input placeholder='Número' type="text" />
+                        <input placeholder='Número' type="text"  />
                     </form>
                     <form>
                         <input type="submit"  />
@@ -48,12 +75,13 @@ export default function Cep(props) {
                 </div>
             </div>
             <footer className="end">
-                <h2>{props.endName}</h2>
+                <h2>Pedro henrique Moreira Sales</h2>
                 <div className="line"></div>
-                <p>Contato: <a href="mailto:xs.salles@gmail.com">{props.email}</a></p>
+                <p>Contato: <a href="mailto:xs.salles@gmail.com">xs.salles@gmail.com</a></p>
             </footer>
         </div>   
     )
-};
+}
+
 
 
